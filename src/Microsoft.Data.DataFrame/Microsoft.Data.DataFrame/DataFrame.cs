c0c3df9f082954682737741078cbedf743c4f1af
Prashanth Govindarajan <prgovi@microsoft.com>
2019-03-05 16:55:21 -0800

    Initial commit of DataFrame

---
+++ src/Microsoft.Data.DataFrame/Microsoft.Data.DataFrame/DataFrame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Apache.Arrow;
+using System.Collections.Generic;
+
+namespace Microsoft.Data.DataFrame
+{
+    public class DataFrame // IDataView eventually
+    {
+        private Table table;
+
+        #region constructors
+        public DataFrame()
+        {
+            table = new Table();
+        }
+
+        // Constructor with a dictionary
+        public DataFrame(IEnumerable<int> columnData, IList<string> columnNames) 
+        {
+            // Is there a way to pass IList<T> as the parameter? Nope. Might need a builder class for this
+        }
+
+        #endregion
+
+        #region DataFrame
+
+        public int NumColumns => table.NumColumns;
+        public int NumRows => table.NumColumns;
+        public void AddColumn(int columnIndex, Column column)
+        {
+            table.AddColumn(columnIndex, column);
+        }
+
+        public void RemoveColumn(int columnIndex)
+        {
+            table.RemoveColumn(columnIndex);
+        }
+
+        public void SetColumn(int columnIndex, Column column)
+        {
+            table.SetColumn(columnIndex, column);
+        }
+        #endregion
+
+        #region Operators
+
+        public Column this[int columnIndex] => table.Column(columnIndex);
+
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
